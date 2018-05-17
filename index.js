aed9bf1d1e11d79e2f00ed3915a78744247c4d79
Wendy She <whshiwen@gmail.com>
2018-05-17 14:20:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ function destructivelyRemoveFirstKitten(name){kittens.shift(`${name}`);return(ki
 function appendKitten(name){var newKittens=[...kittens,`${name}`];return(newKittens)}
 function prependKitten(name){var newKittens=[`${name}`,...kittens];return(newKittens)}
 function removeLastKitten(name){var newKittens=kittens.slice(0,kittens.length-1);return(newKittens)}
-function removeFirstKitten(name){var newKittens=kittens.slice(1);return
+function removeFirstKitten(name){var newKittens=kittens.slice(1);return(newKittens)}
