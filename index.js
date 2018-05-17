ad1fe1e1e1fe6d5aee061526ae07e0a4b5578091
Wendy She <whshiwen@gmail.com>
2018-05-17 14:13:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const kittens=["Milo","Otis","Garfield"]
+function destructivelyAppendKitten(name){kittens.push(`${name}`);return(kittens)}
+function destructivelyPrependKitten(name){kittens.unshift(`${name}`);return(kittens)}
+function destructivelyRemoveLastKitten(name){kittens.pop(`${name}`);return(kittens)}
+function destructivelyRemoveFirstKitten(name){kittens.shift(`${name}`);return(kittens)}
