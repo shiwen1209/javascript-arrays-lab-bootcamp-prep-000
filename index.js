c26714cee533c19c3cb6e0c9b7ae59862c80310a
Wendy She <whshiwen@gmail.com>
2018-05-17 14:19:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ function destructivelyRemoveLastKitten(name){kittens.pop(`${name}`);return(kitte
 function destructivelyRemoveFirstKitten(name){kittens.shift(`${name}`);return(kittens)}
 function appendKitten(name){var newKittens=[...kittens,`${name}`];return(newKittens)}
 function prependKitten(name){var newKittens=[`${name}`,...kittens];return(newKittens)}
-function removeLastKitten(name){var newKittens=kittens.slice(0,kittens.length-1)
+function removeLastKitten(name){var newKittens=kittens.slice(0,kittens.length-1);return(newKittens)}
+function removeFirstKitten(name){var newKittens=kittens.slice(1);return
